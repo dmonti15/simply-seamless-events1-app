d0a8b3f8fa11dfa9f4bbd12754123d3c25608fd2
Daniella Montinola <dmonti15@gmail.com>
2019-01-17 19:51:24 -0600

    Sendgrid addon on heroku updated domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'simplyseamlessevents.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
